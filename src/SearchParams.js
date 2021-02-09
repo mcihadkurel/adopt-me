8d96238c7b6453ddf9a6eb59033f046e0706f4c4
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-02-09 16:13:23 +0100

    Create searc bar

---
+++ src/SearchParams.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SearchParams = () => {
+    const location = "İzmir, TR";
+
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    Location
+                    <input id="location" value={location} placeholder="Location" />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchParams;
