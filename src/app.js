dc21b34fc4b2d429ed68181d937dc0e567ed0dfc
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-02-10 19:10:41 +0100

    SearchParams hook

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { render } from "react-dom";
-import Pet from "./Pet";
+import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
 
 const App = () => {
